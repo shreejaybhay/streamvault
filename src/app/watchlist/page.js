3289316a010725252c94a3d78a307b0864daa5c6
Shree Jaybhay <shreejaybhay26@gmail.com>
2024-07-14 22:57:57 -0700

    Updated

--- src/app/watchlist/page.js
+++ src/app/watchlist/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Watchlists = () => {
     return (
-        <div className='h-[calc(100vh-68px)] p-4 md:p-10 w-full flex items-center justify-center'>
+        <div className='h-[calc(100vh-68px)] p-4 md:p-10 w-full flex items-center justify-center bg-base-200'>
             <div className="flex flex-col w-full space-y-4 md:flex-row md:space-y-0 md:space-x-4">
                 <Link href='/watchlist/movies' className="grid flex-grow h-20 card bg-base-300 rounded-box place-items-center">
                     Movies Watchlist
